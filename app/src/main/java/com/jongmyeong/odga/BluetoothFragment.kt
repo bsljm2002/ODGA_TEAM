7f90faabc55001a7c4874a7412c27c5f3b32cd3e
bsljm <71811724+bsljm2002@users.noreply.github.com>
2021-10-06 21:24:53 +0900

    connect bluetooth
    readable deep model data through bluetooth

--- app/src/main/java/com/jongmyeong/odga/BluetoothFragment.kt
+++ app/src/main/java/com/jongmyeong/odga/BluetoothFragment.kt
@@ -1,59 +1,423 @@
 package com.jongmyeong.odga
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.telephony.SmsManager
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import android.widget.ToggleButton
+import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.UnsupportedEncodingException
+import java.util.*
+import kotlin.collections.ArrayList
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BluetoothFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BluetoothFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var mainActivity: MainActivity
+    private val REQUEST_ENABLE_BT=1
+    private val REQUEST_ALL_PERMISSION= 2
+    private val MY_PERMISSION_REQUEST_SMS = 1001
+    var locationManager : LocationManager? = null
+    private val REQUEST_CODE_LOCATION : Int = 2
+    var currentLocation : String = ""
+    var latitude : Double? = null
+    var longitude : Double? = null
+    private val PERMISSIONS = arrayOf(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+        android.Manifest.permission.BLUETOOTH_SCAN,
+        android.Manifest.permission.BLUETOOTH,
+        android.Manifest.permission.BLUETOOTH_ADMIN,
+        android.Manifest.permission.BLUETOOTH_CONNECT
+    )
+    var s_state = 0
+    var foundDevice:Boolean = false
+    private var mBluetoothStateReceiver: BroadcastReceiver? = null
+    private var bluetoothAdapter: BluetoothAdapter? = null
+
+    var socket: BluetoothSocket? = null
+    var mOutputStream: OutputStream? = null
+    var mInputStream: InputStream? = null
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    val putTxt: MutableLiveData<String> = MutableLiveData("")
+    var targetDevice: BluetoothDevice? = null
+    var currentLatLng: Location? = null
+
+    fun getCurrentLoc(): String {
+        locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        var userLocation: Location = getLatLng()
+        if (userLocation != null) {
+            latitude = userLocation.latitude
+            longitude = userLocation.longitude
+//            Log.d("CheckCurrentLocation", "현재 내 위치 값: $latitude, $longitude")
+
+            var mGeocoder = Geocoder(requireContext(), Locale.KOREAN)
+            var mResultList: List<Address>? = null
+            try {
+                mResultList = mGeocoder.getFromLocation(
+                    latitude!!, longitude!!, 1
+                )
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            if (mResultList != null) {
+                Log.d("CheckCurrentLocation", mResultList[0].getAddressLine(0))
+                currentLocation = mResultList[0].getAddressLine(0)
+                currentLocation = currentLocation.substring(5)
+                return currentLocation
+            }
+        }
+        return (-1).toString()
+    }
+    fun getLatLng() : Location {
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), this.REQUEST_CODE_LOCATION)
+            getLatLng()
+        } else {
+            val locationProvider = LocationManager.GPS_PROVIDER
+            currentLatLng = locationManager?.getLastKnownLocation(locationProvider)
+        }
+        return currentLatLng!!
+    }
+
+    fun bluetoothOnOff(){
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Log.d("bluetoothAdapter","Device doesn't support Bluetooth")
+        }else{
+            if (bluetoothAdapter?.isEnabled == false) { // 블루투스 꺼져 있으면 블루투스 활성화
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            } else{ // 블루투스 켜져있으면 블루투스 비활성화
+                bluetoothAdapter?.disable()
+            }
+        }
+    }
+
+    fun scanDevice(){
+        registerBluetoothReceiver()
+        bluetoothAdapter?.startDiscovery()
+    }
+
+    fun registerBluetoothReceiver(){
+//        intentfilter
+
+        val stateFilter = IntentFilter()
+        stateFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED) //BluetoothAdapter.ACTION_STATE_CHANGED : 블루투스 상태변화 액션
+        stateFilter.addAction(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED)
+        stateFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED) //연결 확인
+        stateFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED) //연결 끊김 확인
+        stateFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED)
+        stateFilter.addAction(BluetoothDevice.ACTION_FOUND) //기기 검색됨
+        stateFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED) //기기 검색 시작
+        stateFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED) //기기 검색 종료
+        stateFilter.addAction(BluetoothDevice.ACTION_PAIRING_REQUEST)
+        mBluetoothStateReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent) {
+                val action = intent.action //입력된 action
+                if (action != null) {
+                    Log.d("Bluetooth action", action)
+                }
+                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                var name: String? = null
+                if (device != null) {
+                    name = device.name //broadcast를 보낸 기기의 이름을 가져온다.
+
+                }
+                when (action) {
+                    BluetoothAdapter.ACTION_STATE_CHANGED -> {
+                        val state = intent.getIntExtra(
+                            BluetoothAdapter.EXTRA_STATE,
+                            BluetoothAdapter.ERROR
+                        )
+                        when (state) {
+                            BluetoothAdapter.STATE_OFF -> {
+                            }
+                            BluetoothAdapter.STATE_TURNING_OFF -> {
+                            }
+                            BluetoothAdapter.STATE_ON -> {
+                            }
+                            BluetoothAdapter.STATE_TURNING_ON -> {
+                            }
+                        }
+                    }
+                    BluetoothDevice.ACTION_ACL_CONNECTED -> {
+
+
+                    }
+                    BluetoothDevice.ACTION_BOND_STATE_CHANGED -> {
+                    }
+                    BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
+                        //디바이스가 연결 해제될 경우
+
+
+                    }
+                    BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
+
+                    }
+                    BluetoothDevice.ACTION_FOUND -> {
+                        if (!foundDevice) {
+                            val device_name = device!!.name
+                            val device_Address = device.address
+                            Log.d("nameD","${device_name},${device_Address}")
+                            //블루투스 기기 이름의 앞글자가 "RNM"으로 시작하는 기기만을 검색한다
+                            if (device_name != null && device_name.length > 4) {
+                                if (device_name.substring(0, 3) == "ODG") {
+                                    targetDevice = device
+                                    foundDevice = true
+                                    //찾은 디바이스에 연결한다.
+                                    connectToTargetedDevice(targetDevice)
+
+                                }
+                            }
+                        }
+                    }
+                    BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
+                        if (!foundDevice) {
+                            //Toast massage
+                        }
+                    }
+
+                }
+            }
+        }
+        //리시버 등록
+        context?.applicationContext?.registerReceiver(
+            mBluetoothStateReceiver,
+            stateFilter
+        )
+
+    }
+
+    @ExperimentalUnsignedTypes
+    private fun connectToTargetedDevice(targetedDevice: BluetoothDevice?) {
+
+
+        val thread = Thread {
+            //선택된 기기의 이름을 갖는 bluetooth device의 object
+            val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+            try {
+                // 소켓 생성
+                socket = targetedDevice?.createRfcommSocketToServiceRecord(uuid)
+
+                socket?.connect()
+
+                /**
+                 * After Connect Device
+                 */
+//                connected.postValue(true)
+                mOutputStream = socket?.outputStream
+                mInputStream = socket?.inputStream
+                // 데이터 수신
+                beginListenForData()
+
+            } catch (e: java.lang.Exception) {
+                // 블루투스 연결 중 오류 발생
+                e.printStackTrace()
+
+                // connectError.postValue(Event(true))
+                try {
+                    socket?.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        //연결 thread를 수행한다
+        thread.start()
+    }
+    fun beginListenForData() {
+        val mWorkerThread = Thread(Runnable {
+            while (!Thread.currentThread().isInterrupted) {
+                try {
+                    val bytesAvailable = mInputStream?.available()
+                    val DB_NAME = "sqlite.sql"
+                    val DB_VERSION = 1
+                    val helper = SqliteHelper(requireContext(), DB_NAME, DB_VERSION)
+                    val phones = helper.selectPhoneBook()
+
+                    if (bytesAvailable != null) {
+                        if (bytesAvailable > 0) { //데이터가 수신된 경우
+                            val packetBytes = ByteArray(bytesAvailable)
+
+                            mInputStream?.read(packetBytes)
+                            /**
+                             * 한 버퍼 처리
+                             */
+                            /**
+                             * 한 버퍼 처리
+                             */
+
+                            // Byte -> String
+                            val s = String(packetBytes,Charsets.UTF_8)
+                            //수신 String 출력
+                            putTxt.postValue(s)
+
+                            /**
+                             * 한 바이트씩 처리
+                             */
+                            /**
+                             * 한 바이트씩 처리
+                             */
+                            for (i in 0 until bytesAvailable) {
+                                val b: Byte = packetBytes[i]
+
+                                val b_string = b.toInt()
+                                Log.d("b_string", "${b_string}")
+
+                                if(b_string == 82) {
+                                    s_state=1
+                                }
+
+                                Log.d("s_state",""+s_state)
+
+                                if(s_state==1) {
+                                    val handler = Handler(Looper.getMainLooper())
+                                    handler.postDelayed({
+                                        for (i in phones.indices) {
+                                            try {
+                                                var addr:String = getCurrentLoc()
+                                                val smsManager: SmsManager = SmsManager.getDefault()
+                                                for (i in phones.indices) {
+                                                    Log.d("address","${addr}")
+//                                                        smsManager?.sendTextMessage("${phones[i]?.fphone}", null, "사고발생 위치 정보(위도/경도) : ${address}", null, null)
+                                                    Toast.makeText(activity, "Message sent", Toast.LENGTH_SHORT).show()
+                                                    break
+                                                }
+                                            } catch (e: Exception) {
+                                                Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
+                                                e.printStackTrace()
+                                            }
+                                        }
+                                    }, 0)
+                                }
+////
+                            }
+                        }
+
+                    }
+                } catch (e: UnsupportedEncodingException) {
+                    e.printStackTrace()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        })
+        //데이터 수신 thread 시작
+        mWorkerThread.start()
+    }
+
+    // Permission check
+    private fun hasPermissions(context: Context?, permissions: Array<String>): Boolean {
+        for (permission in permissions) {
+            if (context?.let { ActivityCompat.checkSelfPermission(it, permission) }
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_ALL_PERMISSION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(activity, "Permissions granted!", Toast.LENGTH_SHORT).show()
+                } else {
+                    requestPermissions(permissions, REQUEST_ALL_PERMISSION)
+                    Toast.makeText(activity, "Permissions must be granted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_bluetooth, container, false)
+        val bleOnOffBtn: ToggleButton = view.findViewById(R.id.ble_on_off_btn)
+        val scanBtn: Button = view.findViewById(R.id.scanBtn)
+        val bluetoothManager =  activity?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+
+        bluetoothAdapter = bluetoothManager.adapter
+        viewManager = LinearLayoutManager(activity)
+
+        if(bluetoothAdapter!=null){
+            if(bluetoothAdapter?.isEnabled==false){
+                bleOnOffBtn.isChecked = true
+                scanBtn.isVisible = false
+            } else{
+                bleOnOffBtn.isChecked = false
+                scanBtn.isVisible = true
+            }
+        }
+
+        bleOnOffBtn.setOnCheckedChangeListener { _, isChecked ->
+            bluetoothOnOff()
+            scanBtn.visibility = if (scanBtn.visibility == View.VISIBLE){ View.INVISIBLE } else{ View.VISIBLE }
+        }
+
+        scanBtn.setOnClickListener { v:View? -> // Scan Button Onclick
+            if (!hasPermissions(activity, PERMISSIONS)) {
+                requestPermissions(PERMISSIONS, REQUEST_ALL_PERMISSION)
+            }
+
+            scanDevice()
+
+        }
+//        checkSMS()
+//        initRecycler()
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bluetooth, container, false)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BluetoothFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BluetoothFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }

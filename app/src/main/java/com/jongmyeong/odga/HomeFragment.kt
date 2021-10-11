229fd6883f7f5290cf274002350e3b48d9878f63
bsljm <71811724+bsljm2002@users.noreply.github.com>
2021-10-11 14:40:18 +0900

    kakaoMapWebView
    홈페이지 카카오 맵 웹뷰로 로드

--- app/src/main/java/com/jongmyeong/odga/HomeFragment.kt
+++ app/src/main/java/com/jongmyeong/odga/HomeFragment.kt
@@ -1,13 +1,29 @@
 package com.jongmyeong.odga
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.app.AlertDialog
+import net.daum.mf.map.api.MapView
 
+import android.content.ComponentCallbacks
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.*
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.net.URISyntaxException
+import java.util.jar.Manifest
 
 
 class HomeFragment : Fragment() {
@@ -25,6 +41,72 @@ class HomeFragment : Fragment() {
 
         val btnMusic : ImageButton = view.findViewById(R.id.btnMusic)
 
+        val webview : WebView = view.findViewById(R.id.webview)
+
+        webview.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            //settings.setSupportMultipleWindows(true)
+
+        }
+
+        webview.webChromeClient = WebChromeClient()
+
+        webview.webChromeClient = object : WebChromeClient(){
+
+            override fun onGeolocationPermissionsShowPrompt(
+                origin:String?,
+                callback: GeolocationPermissions.Callback?
+            )
+
+            {
+                super.onGeolocationPermissionsShowPrompt(origin, callback)
+                callback?.invoke(origin, true, false)
+            }
+
+
+        }
+        webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                if (url != null && url.startsWith("intent://")) {
+                    try {
+                        val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+                        val existPackage =
+                            activity?.packageManager?.getLaunchIntentForPackage(intent.getPackage()!!)
+                        if (existPackage != null) {
+                            startActivity(intent)
+                        } else {
+                            val marketIntent = Intent(Intent.ACTION_VIEW)
+                            marketIntent.data =
+                                Uri.parse("market://details?id=" + intent.getPackage()!!)
+                            startActivity(marketIntent)
+                        }
+                        return true
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                } else if (url != null && url.startsWith("market://")) {
+                    try {
+                        val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+                        if (intent != null) {
+                            startActivity(intent)
+                        }
+                        return true
+                    } catch (e: URISyntaxException) {
+                        e.printStackTrace()
+                    }
+
+                }
+                view.loadUrl(url)
+                return false
+            }
+        }
+
+        webview.loadUrl("https://map.kakao.com/")
+
+
+
         btnMusic.setOnClickListener(object :View.OnClickListener {
             override fun onClick(v: View?) {
                 // 버튼 클릭시 화면 전환
@@ -39,7 +121,9 @@ class HomeFragment : Fragment() {
 
         })
 
-
         return view
     }
+
+
+
 }
